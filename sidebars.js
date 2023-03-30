49cfbdcc59cfe96dfd7024f0ee67572e2ff0fcd2
vwumumu <vwumumu@gmail.com>
2023-03-30 23:01:01 +0800

    add meeting of 20230330

--- sidebars.js
+++ sidebars.js
@@ -84,6 +84,7 @@ const sidebars = {
       type: "category",
       label: "5.周会",
       items: [
+        "weekly-meeting/20230330",
         "weekly-meeting/20230316",
         "weekly-meeting/20230309",
         "weekly-meeting/20230223",
